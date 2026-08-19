813df1b4fe96f15f75a19adb6f79604bc657f47a
Miirten <metalguitaristpro@hotmail.com>
2026-08-19 14:05:05 -0600

    Main page
    Created a main page so Vercel doesn't freak

---
+++ react/app/page.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const projects = [
+    {
+        title: "React Counter Page",
+        description: "A page with buttons that count how many times they are clicked. You might find a secret if you click enough.",
+        href: "/react_asssignment_1"
+    }
+];
+
+export default function Page() {
+    return (
+        <main>
+            <h1>My Projects</h1>
+            <p>Select a project to view it.</p>
+
+            <nav aria-label="Project Navigation">
+                <ul>
+                    {projects.map((projects) => (
+                        <li key={project.href}>
+                            <Link href={project.href}>
+                            {project.title}
+                            </Link>
+
+                            <p>{project.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </main>
+    );
+}
