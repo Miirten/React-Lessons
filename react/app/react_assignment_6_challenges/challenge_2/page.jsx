72d80e2f3953ef9594b20fb165632dae5a6b4e61
Miirten <metalguitaristpro@hotmail.com>
2026-08-28 14:21:23 -0600

    Update page.jsx

--- react/app/react_assignment_6_challenges/challenge_2/page.jsx
+++ react/app/react_assignment_6_challenges/challenge_2/page.jsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Toggle() {
   const [isOn, setIsOn] = useState(false);
 
-  function handleClick() {
-    setIsOn((currentIsOn) => !currentIsOn);
-  }
-    return <button onClick={handleClick}>{isOn ? "On" : "Off"}</button>;
-
+  return (
+    <button onClick={() => {
+      setIsOn(!isOn);
+    }}>
+      {isOn ? 'On' : 'Off'}
+    </button>
+  );
 }
